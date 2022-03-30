26ba01b99c1bfcae2e8cb130d45d224d5672bb59
Jarrod Medrano <jmedran@gmail.com>
2022-03-30 05:42:04 -0500

    add replacer gh-pages

--- next.config.js
+++ next.config.js
@@ -5,4 +5,9 @@ const nextConfig = {
   /* config options here */
 };
 
-module.exports = nextConfig;
+module.exports = {
+  env: {
+    PUBLIC_URL: "https://jarrodmedrano.github.io/mit-1",
+    assetPrefix: "./",
+  },
+};
