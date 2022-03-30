e05554e4437c5de44665607e36355aca72fb0cc9
Jarrod Medrano <jmedran@gmail.com>
2022-03-30 10:50:34 -0500

    fix link

--- components/header.js
+++ components/header.js
@@ -14,11 +14,11 @@ export const Header = () => {
   return (
     <header className="mb-auto">
       <div>
-        <Link className="text-white" href="/">
+        <Link className="text-white" href="/mit-1">
           <h3 className="float-md-start mb-0">Jarrod Medrano</h3>
         </Link>
         <nav className="nav nav-masthead justify-content-center float-md-end">
-          <Link aria-current="page" href="/">
+          <Link aria-current="page" href="/mit-1">
             <a className="nav-link">Home</a>
           </Link>
           <Link className="nav-link" aria-current="page" href="/portfolio">
