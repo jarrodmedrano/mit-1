40127bc0b60ad62d1cd0f518db46e6ded319bf8e
Jarrod Medrano <jmedran@gmail.com>
2022-03-30 05:19:52 -0500

    config

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { useRouter } from "next/router";
 import { Header } from "../components/header";
 
 export default function Document() {
